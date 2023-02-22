178010ff7d7168bbb71b3fbd1ba4dbf16021ee07
alexlu0917 <alexlu0917.dev@gmail.com>
2023-02-22 02:35:51 -0600

    add middleware

--- middleware.js
+++ middleware.js
@@ -1,5 +1,17 @@
 const VALID_KEYS_PATH = __dirname + '/valid-keys.txt';
 
 module.exports = function (req, res, next) {
+    const apiKey = req.get('X-API-Key');
+    if (!apiKey) {
+        res.status(401).send('No API key provided');
+        return;
+    }
 
+    const validKeys = fs.readFileSync(VALID_KEYS_PATH, 'utf8').split(os.EOL);
+    if (!validKeys.includes(apiKey)) {
+        res.status(401).send('Invalid API key');
+        return;
+    }
+
+    next();
 };
